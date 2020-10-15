d1ca8e6f1e17d9ec9f173f89f59b366b8c86ca6f
David <simma.david@gmail.com>
2020-10-15 17:12:03 +0200

    Fibonacci fertig

--- Regression/src/Fibonacci.java
+++ Regression/src/Fibonacci.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Anzahl von Fibonacci Zahlen: ");
+        System.out.println(fibonacci(reader.nextInt()));
+
+
+    }
+    public static int fibonacci(int anzahl){
+        if(anzahl <=1){
+            return anzahl;
+        }
+        return fibonacci(anzahl -1) + fibonacci(anzahl -2);
+    }
 }
