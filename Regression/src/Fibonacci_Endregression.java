f6ebf78232ebf5136746c8a710de0da0e2cfd61f
David <simma.david@gmail.com>
2020-11-03 12:00:36 +0100

    endregression fibonacci fertig

---
+++ Regression/src/Fibonacci_Endregression.java
@@ -0,0 +1,12 @@
+public class Fibonacci_Endregression {
+    public static void main(String[] args) {
+        System.out.println(fib1(8));
+    }
+    public static int fib1(int n){
+        return fib2(n-1, 1, 0);
+    }
+    public static int fib2(int n, int einz, int zwei){
+        if (n<=1) return einz+zwei;
+        return fib2(n-1, einz+zwei, einz);
+    }
+}
